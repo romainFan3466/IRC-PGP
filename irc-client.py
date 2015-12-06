66b74c0d6e6db2a56d4c1f7e4aaad4778690c493
Romain Fanara <romain.fanara@gmail.com>
2015-12-06 20:12:21 +0000

    arg parser added

--- irc-client.py
+++ irc-client.py
@@ -9,14 +9,52 @@ import sys
 import socket
 import irc.client
 import jaraco.logging
+import argparse
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--mode",
+        help="Set the bot mode : listener or sender. Default is listener",
+        default="listener"
+    )
+    parser.add_argument(
+        "-ho",
+        "--host",
+        help="IRC Server host. Default is Romain's server host",
+        default="51.255.38.201"
+    )
+    parser.add_argument(
+        "-ch",
+        "--channel",
+        help="IRC Server channel to join. Need to start by # "
+    )
+    parser.add_argument(
+        "-nick",
+        "--nickname",
+        help="Client nickname"
+    )
+    parser.add_argument(
+        "-po",
+        "--port",
+        help="IRC Server port. Default is 6666",
+        type=int,
+        default=6666
+    )
+    parser.add_argument(
+        "-pw",
+        "--password",
+        help="IRC Server password if required"
+    )
 
+    return parser.parse_args()
 
 def listen_to(connection):
     soc = connection.socket
     buf = ''
     while connection.is_connected():
-        buf += soc.recv(1024).decode("utf-8")
+        buf = soc.recv(1024).decode("utf-8")
         print(buf)
 
 def send_it(connection, target):
@@ -29,23 +67,27 @@ def send_it(connection, target):
 
 
 def main():
-    
-    port = 6666
-    nickname = "mybot"
-    target = "#1"
-    server = "127.0.0.1"
+
+    args = parse_args()
+    port = args.port
+    nickname = args.nickname
+    target = args.channel
+    server = args.host
+    password = None if not args.password else args.password
+
 
     reactor = irc.client.Reactor()
     try:
-        c = reactor.server().connect(server, port, nickname)
+        c = reactor.server().connect(server, port, nickname,password=password)
         c.join(target)
 
-
-        # Uncomment/comment the two following lines to switch the server listener/sender
-
-        #listen_to(c)
-        send_it(c, target)
-
+        if args.mode == "listener":
+            listen_to(c)
+        elif args.mode == "sender":
+            send_it(c, target)
+        else:
+            print("no mode specified")
+            raise SystemExit(1)
 
     except irc.client.ServerConnectionError:
         print(sys.exc_info()[1])
