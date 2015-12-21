c24c9ea118ab0810d267d9d3b31a33218c55fe4c
Romain Fanara <romain.fanara@gmail.com>
2015-12-21 22:36:35 +0100

    message static method

--- app/models/message.py
+++ app/models/message.py
@@ -1,3 +1,17 @@
 
 class Message:
     pass
+
+
+    @staticmethod
+    def rawParse(encodedMessage:str=""):
+        pt = {}
+        if "PRIVMSG" in encodedMessage:
+            raw_user, type, target, raw_message = encodedMessage.split(sep=" ",maxsplit=3)
+            user = raw_user[1:]
+            user = user.split(sep="!")[0]
+            message = raw_message[1:]
+            pt["username"] = '<span style="color:blue; font-weight: bolder;">' + user + "</span>"
+            pt["target"] = target
+            pt["message"] = message
+        return pt
