d09507690ef9d03b7113c376716b1bd6666a5ada
JenniferRyan <jenniferryan.ie@gmail.com>
2016-01-25 22:35:13 +0000

    Updated API code

--- api/api.py
+++ api/api.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template, request, session, jsonify
-from flask_restful import Resource 
+import DBcm
+from flask import Flask, request, jsonify, session
+from flask_restful import reqparse
 from functools import wraps
-from json import dumps
 
 app = Flask(__name__)
-#api = Api(app)	
 
-import DBcm
 
 DBconfig = {'host': '127.0.0.1',
-			'user': 'ircUSER',
-			'password': 'ircPW',
-			'database': 'ircDB' }
-			
+            'user': 'ircUSER',
+            'password': 'ircPW',
+            'database': 'ircDB'}
+
+
+# session to check if logged in
+def check_login(func):
+    @wraps(func)
+    def wrapped_function(*args, **kwargs):
+        if 'logged_in' in session:
+            return func(*args, **kwargs)
+        return not_authorised(403)
+    return wrapped_function
+
+
 @app.route('/users/login', methods=['POST'])
-def main():
+def api_login():
     try:
         # Parse the arguments
         parser = reqparse.RequestParser()
@@ -24,21 +33,24 @@ def main():
 
         _userLogin = args['login']
         _userPassword = args['password']
-        #cursor.callproc('sp_AuthenticateUser',(_userLogin,))
-        
+
         with DBcm.UseDatabase(DBconfig) as cursor:
             cursor.execute("""SELECT * FROM Users WHERE login=%s """, (_userLogin,))
             data = cursor.fetchall()
-            items_list=[]
+            items_list = []
             for item in data:
-               if(str(item[3])==_userPassword):
-                   i = {
-                       'User Id':item[0],
-                       }
-               items_list.append(i)
-            result = jsonify(items=items_list)
-            result.status_code = 200
-        return result     
+                if item[3] == _userPassword:
+                    # session['logged_in'] = True
+                    i = {
+                       'User Id': item[0],
+                       'User Name': item[1],
+                        }
+                    items_list.append(i)
+                    result = jsonify(items=items_list)
+                    result.status_code = 200
+                    return result
+                else:
+                    return not_authorised(403)
 
     except Exception as e:
         return {'error': str(e)}
@@ -46,50 +58,111 @@ def main():
 
 # On api login, send new(randomly generated) public key to API and then store in DB
 @app.route('/publicKeys/update', methods=['POST', 'PUT'])
+# @check_login
 def api_update():
-	return json.dumps({""}), 200 , {'Content-Type': 'applicaion/json'}
+    try:
+        parser = reqparse.RequestParser()
+        parser.add_argument('public_key')
+        parser.add_argument('login', help='User login name for Authentication')
+        args = parser.parse_args()
+
+        _publicKey = args['public_key']
+        _userLogin = args['login']
+
+        with DBcm.UseDatabase(DBconfig) as cursor:
+            cursor.execute(""" UPDATE Users
+                           SET publicKey = %s
+                           WHERE login = %s """, (_publicKey, _userLogin,))
+            cursor.execute(""" SELECT publicKey
+                           FROM Users
+                           WHERE publicKey = %s """, (_publicKey,))
+            data = cursor.fetchone()
+            if data is not None:
+                i = {
+                    'Public_key': data[0],
+                    }
+                result = jsonify(i)
+                result.status_code = 200
+            else:
+                return not_acceptable(406)
+        return result
+
+    except Exception as e:
+        return {'error': str(e)}
 
 
 # Send request to the DB to retrieve all stored public keys	
 @app.route('/publicKeys/getAll', methods=['GET'])
-def api_getAll():
-	try:
-		with DBcm.UseDatabase(DBconfig) as cursor: 
-			cursor.execute(""" SELECT * FROM Users """)
-			data = cursor.fetchall()
-			items_list=[];
-			for item in data:
-				i = {
-					'Id':item[0],
-					'Public_key':item[4]
-					}
-				items_list.append(items_list)
-			result = jsonify(i)
-			result.status_code = 200
+# @check_login
+def api_get_all():
+    try:
+        with DBcm.UseDatabase(DBconfig) as cursor:
+            cursor.execute(""" SELECT * FROM Users """)
+            data = cursor.fetchall()
+            items_list = []
+            for item in data:
+                i = {
+                    'Public_key': item[4],
+                    }
+                items_list.append(items_list)
+            result = jsonify(i)
+            result.status_code = 200
+        return result
 
-		return result #"Success! "#{'StatusCode':'200','Items':items_list}
+    except Exception as e:
+        return {'error': str(e)}
 
-	except Exception as e:
-		return {'error': str(e)}
+
+@app.route('/ircServers/connect', methods=['POST'])
+@check_login
+def api_connect():
+    try:
+        parser = reqparse.RequestParser()
+        parser.add_argument('host', help="IRC Server host. Default is Romain's server host")
+        parser.add_argument('port', help='User login name for Authentication')
+        args = parser.parse_args()
+
+        _host = args['host']
+        _port = args['port']
+
+        with DBcm.UseDatabase(DBconfig) as cursor:
+            cursor.execute(""" INSERT INTO IrcServers (host, port)
+                           VALUES (%s, %s) """, (_host, _port,))
+            cursor.execute(""" SELECT host, port
+                           FROM IrcServers
+                           WHERE host = %s
+                           AND port = %s""", (_host, _port,))
+            data = cursor.fetchall()
+            if data is not None:
+                i = {
+                    'Host': data[0],
+                    'Port': data[1],
+                    }
+            result = jsonify(ircServer=i)
+            result.status_code = 200
+
+        return result
+
+    except Exception as e:
+        return {'error': str(e)}
+
+
+@app.route('/ircServers/join/<channel>', methods=['GET'])
+@check_login
+def api_join_channel():
+    return ""
 
 
 @app.route('/users/logout')
-def api_logout():
-# need to erase the stored public key for this IRC Client
-    #session.pop('logged_in')
+def api_logout():  # need to erase the stored public key for this IRC Client
+    session.pop('logged_in')
     return "GOODBYE..."
-	
-@app.route('/ircServers/connect', methods=['POST'])
-def api_connect():
-	return ""	
 
-@app.route('/ircServers/join/<channel>', methods=['GET'])
-def api_joinChannel():
-	return ""	
 
 @app.route('/errorLogs/report', methods=['POST'])
-def api_report():
-	return ""
+@check_login
+def api_error_reports():
+    return ""
 
 
 @app.errorhandler(404)
@@ -103,7 +176,33 @@ def not_found(error=None):
 
     return resp
 
+
+@app.errorhandler(403)
+def not_authorised(error=None):
+    message = {
+            'status': 403,
+            'message': 'Unauthorised: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 403
+
+    return resp
+
+
+@app.errorhandler(406)
+def not_acceptable(error=None):
+    message = {
+            'status': 406,
+            'message': 'Not Acceptable  :' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 406
+
+    return resp
+
 app.error_handler_spec[None][404] = not_found
+app.error_handler_spec[None][403] = not_authorised
+app.error_handler_spec[None][406] = not_acceptable
 
 if __name__ == '__main__':
     app.run(debug=True)
