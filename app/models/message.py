8b8e596f7e27655f6993faef5b54b2e3ac9e934c
Romain Fanara <romain.fanara@gmail.com>
2016-02-04 02:37:09 +0000

    message formatting and encryption

--- app/models/message.py
+++ app/models/message.py
@@ -1,6 +1,47 @@
 
+
+from app.handlers.pgpHandler import PgpHandler
+from simplejson import JSONEncoder
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto import Random
+
 class Message:
-    pass
+
+    plaintext = ""
+    signature = ""
+    publicKey = ""
+
+    def __init__(self, plaintext, publicKey=""):
+        self.plaintext = plaintext
+        self.publicKey = publicKey
+
+
+    def sign(self, privateKey):
+        self.signature = PgpHandler.sign(privateKey, self.plaintext)
+
+
+    def verify(self):
+        return PgpHandler.verify(self.plaintext, self.publicKey,self.signature)
+
+
+    def __JSONencode(self):
+        return JSONEncoder().encode(self.__dict__)
+
+    def __getEncrypt(self):
+        encoded = self.__JSONencode()
+        password = PgpHandler.generateKey(32)
+        key = PgpHandler.generateHash(password)
+        return {
+            "cipher" : PgpHandler.AESencrypt(self.plaintext, key),
+            "AESkey" : password,
+            "receiver_username" : "receiver" # at the final with rsa :
+        }
+
+    def getFinal(self):
+        encrypted = self.__getEncrypt()
+        return JSONEncoder().encode(encrypted)
 
 
     @staticmethod
@@ -15,3 +56,9 @@ class Message:
             pt["target"] = target
             pt["message"] = message
         return pt
+
+    @staticmethod
+    def exportNames(message, pattern):
+        names = []
+        names = message.split(sep=pattern)
+        return names[1].split(sep=" ")
